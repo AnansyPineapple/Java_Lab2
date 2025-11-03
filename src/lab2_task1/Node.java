e11c8b928fe4d689dfe730703c778caf59fbe3de
Maria_K <anansy.pineapple@gmail.com>
2025-11-04 04:57:15 +0500

    Now only eng keyboard + setters

--- src/lab2_task1/Node.java
+++ src/lab2_task1/Node.java
@@ -4,18 +4,17 @@ package lab2_task1;
  * @author Maria Khismatullova
  */
 public class Node {
-    /** Constant number of latin, russian letters and numbers */
-    private static final int ALPHABET_SIZE = 69;
-    /** children[0..ALPHABET_SIZE-1] are the child nodes */
+    /** Constant number of english keyboard letters, digits and symbols */
+    private static final int KEYBOARD_SIZE = 94;
+    /** children[0..KEYBOARD_SIZE-1] are the child nodes */
     private Node[] children;
     
     /** An indicator of whether it is an ending letter */
     private boolean terminal;
     
-    /** Constructor: an instance with a potential alphabet-size number of children and supposedly non-terminal
-     */
+    /** Constructor: an instance with a potential keyboard-size number of children and supposedly non-terminal */
     public Node() {
-	this.children = new Node[ALPHABET_SIZE];
+	this.children = new Node[KEYBOARD_SIZE];
 	this.terminal = false;
     }
     
@@ -25,7 +24,7 @@ public class Node {
     }
     
     /** Return a certain child node
-     * Precondition: index in 0..68
+     * Precondition: index in 0..93
      * @param index index of child node in array
      */
     public Node getChild(int index) {
@@ -35,5 +34,13 @@ public class Node {
     /** Return whether letter is ending */
     public boolean isTerminal() {
 	return terminal;
+    }
+    
+    public void setChild(int index) {
+	this.children[index] = new Node();
+    }
+    
+    public void setTerminal(boolean terminal) {
+	this.terminal = terminal;
     }
 }
