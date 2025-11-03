71bf77cb23765db4bc11747a05bf68291402f62f
Maria_K <anansy.pineapple@gmail.com>
2025-11-03 22:27:19 +0500

    init

---
+++ src/lab2_task1/Trie.java
@@ -0,0 +1,11 @@
+package lab2_task1;
+
+/**
+ * An object of class Trie represents a prefix tree
+ * @author Maria Khismatullova
+ */
+public class Trie {
+    public static void main(String args[]) {
+	System.out.println("Hello, World!");
+    }
+}
